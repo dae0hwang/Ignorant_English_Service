c5658e2e3cbe2b5dbc6c11052b3a2bc0a3d93aaa
geung <geungan9@gmail.com>
2023-02-14 22:13:10 +0900

    feat: add jar and property file

--- API/src/main/java/hello/api/ApiApplication.java
+++ API/src/main/java/hello/api/ApiApplication.java
@@ -1,8 +1,8 @@
 package hello.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class ApiApplication {
 
