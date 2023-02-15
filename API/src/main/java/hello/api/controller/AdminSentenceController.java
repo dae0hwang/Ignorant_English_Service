a1617707d9ced1cfcdcd97b4400a9c25eeaa99e4
geung <geungan9@gmail.com>
2023-02-15 22:45:30 +0900

    관리자 문장 등록 셀렉트 박스 정보 가져오기 API 기능 추가

--- API/src/main/java/hello/api/controller/AdminSentenceController.java
+++ API/src/main/java/hello/api/controller/AdminSentenceController.java
@@ -3,9 +3,11 @@ package hello.api.controller;
 import hello.api.dto.AdminSentenceRequest;
 import hello.api.dto.AdminSentenceSuccess;
 import hello.api.service.AdminSentenceService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +24,16 @@ public class AdminSentenceController {
     public ResponseEntity<AdminSentenceSuccess> addSentence(@RequestBody AdminSentenceRequest request) {
         adminSentenceService.saveSentence(request.getKorean(), request.getEnglish(),
             request.getGrammar(), request.getSituation());
-        AdminSentenceSuccess success = new AdminSentenceSuccess(201, null, null);
+        AdminSentenceSuccess success = new AdminSentenceSuccess(201, null, null, null, null);
         return new ResponseEntity<>(success, HttpStatus.CREATED);
     }
 
+    @GetMapping("/selection")
+    public ResponseEntity<AdminSentenceSuccess> findSelectValue() {
+        List<String> grammarValues = adminSentenceService.findGrammarValues();
+        List<String> situationValues = adminSentenceService.findSituationValues();
+        AdminSentenceSuccess success = new AdminSentenceSuccess(200, null, null, grammarValues,
+            situationValues);
+        return new ResponseEntity<>(success, HttpStatus.OK);
+    }
 }
