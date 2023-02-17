ec463893ed98353e0022256aa49ad7f77ba77423
geung <geungan9@gmail.com>
2023-02-17 15:19:21 +0900

    관리자 문장 등록 에러 리스폰스 TEST

--- API/src/test/java/hello/api/controller/AdminSentenceControllerTest.java
+++ API/src/test/java/hello/api/controller/AdminSentenceControllerTest.java
@@ -1,5 +1,6 @@
 package hello.api.controller;
 
+import static hello.api.enumforexception.AdminSentenceExceptionEnum.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
@@ -8,12 +9,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hello.api.dto.AdminSentenceSuccess;
+import hello.api.dto.ErrorResponse;
 import hello.api.enumforentity.Grammar;
 import hello.api.enumforentity.Situation;
+import hello.api.enumforexception.AdminSentenceExceptionEnum;
+import hello.api.exceptionadvice.AdminSentenceExceptionAdvice;
+import hello.api.interceptor.ExceptionResponseInterceptor;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,6 +32,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @ActiveProfiles("test")
 @SpringBootTest
+@Slf4j
 class AdminSentenceControllerTest {
 
     @Autowired
@@ -37,7 +45,10 @@ class AdminSentenceControllerTest {
     void beforeEach() {
         objectMapper = new ObjectMapper();
         baseUrl = "/api/admin/sentence";
-        mockMvc = MockMvcBuilders.standaloneSetup(adminSentenceController).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(adminSentenceController)
+                .setControllerAdvice(new AdminSentenceExceptionAdvice())
+                .addInterceptors(new ExceptionResponseInterceptor())
+                .build();
     }
 
     @Test
@@ -54,6 +65,69 @@ class AdminSentenceControllerTest {
                 .contentType("application/json")
                 .content(requestJson))
             .andExpect(status().isCreated())
+            .andExpect(content().json(responseContent))
+            .andDo(log());
+    }
+
+    @Test
+    void addSentenceException1() throws Exception {
+        //given
+        String url = baseUrl + "/add";
+        String requestJson = "{\"korean\":\"\", \"english\":\"english\", "
+            + "\"grammar\":\"HAVE PP\", \"situation\":\"NO\" }";
+        ErrorResponse error = new ErrorResponse(ADD_SENTENCE_STRING_BLANK.getErrorType(),
+            ADD_SENTENCE_STRING_BLANK.getErrorTitle(), 400,
+            ADD_SENTENCE_STRING_BLANK.getErrormessage(), "/api/admin/sentence/add");
+        String responseContent = objectMapper.writeValueAsString(error);
+        log.info(responseContent);
+        //when
+        //then
+        mockMvc.perform(post(url)
+                .contentType("application/json")
+                .content(requestJson))
+            .andExpect(status().isBadRequest())
+            .andExpect(content().json(responseContent))
+            .andDo(log());
+    }
+
+    @Test
+    void addSentenceException2() throws Exception {
+        //given
+        String url = baseUrl + "/add";
+        String requestJson = "{\"korean\":\"korean\", \"english\":\"english\", "
+            + "\"grammar\":\"NO-MATCH\", \"situation\":\"NO\" }";
+        ErrorResponse error = new ErrorResponse(NO_MATCH_GRAMMAR_ENUM.getErrorType(),
+            NO_MATCH_GRAMMAR_ENUM.getErrorTitle(), 400,
+            NO_MATCH_GRAMMAR_ENUM.getErrormessage(), "/api/admin/sentence/add");
+        String responseContent = objectMapper.writeValueAsString(error);
+        log.info(responseContent);
+        //when
+        //then
+        mockMvc.perform(post(url)
+                .contentType("application/json")
+                .content(requestJson))
+            .andExpect(status().isBadRequest())
+            .andExpect(content().json(responseContent))
+            .andDo(log());
+    }
+
+    @Test
+    void addSentenceException3() throws Exception {
+        //given
+        String url = baseUrl + "/add";
+        String requestJson = "{\"korean\":\"korean\", \"english\":\"english\", "
+            + "\"grammar\":\"HAVE PP\", \"situation\":\"NO-MATCH\" }";
+        ErrorResponse error = new ErrorResponse(NO_MATCH_SITUATION_ENUM.getErrorType(),
+            NO_MATCH_SITUATION_ENUM.getErrorTitle(), 400,
+            NO_MATCH_SITUATION_ENUM.getErrormessage(), "/api/admin/sentence/add");
+        String responseContent = objectMapper.writeValueAsString(error);
+        log.info(responseContent);
+        //when
+        //then
+        mockMvc.perform(post(url)
+                .contentType("application/json")
+                .content(requestJson))
+            .andExpect(status().isBadRequest())
             .andExpect(content().json(responseContent))
             .andDo(log());
     }
