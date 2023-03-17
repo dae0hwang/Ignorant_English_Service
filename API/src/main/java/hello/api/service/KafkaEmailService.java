8c5c0522434170421f1cabfc07ad5e054798a181
geung <geungan9@gmail.com>
2023-03-17 14:12:26 +0900

    kafka email json

--- API/src/main/java/hello/api/service/KafkaEmailService.java
+++ API/src/main/java/hello/api/service/KafkaEmailService.java
@@ -3,12 +3,14 @@ package hello.api.service;
 import hello.api.dto.EmailDto;
 import hello.api.dto.StockChange;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class KafkaEmailService {
 
     private static final String TOPIC = "email";
@@ -16,11 +18,12 @@ public class KafkaEmailService {
 
     public void sendMessage(String message) {
         EmailDto emailDto = new EmailDto(message);
+        log.info("email produce ={}", emailDto);
         kafkaTemplate.send(TOPIC, emailDto);
     }
 
     @KafkaListener(topics = TOPIC, groupId = "foo", containerFactory = "kafkaEmailListener")
-    public void consume(StockChange stockChange) {
-        System.out.printf("Consumed message : %s%n", stockChange.getYyyymmdd());
+    public void consume(EmailDto emailDto) {
+        log.info("email consume ={}", emailDto);
     }
 }
