97e2d55b863de49468ccf7956ff09a6209a787ec
geung <geungan9@gmail.com>
2023-03-17 09:59:37 +0900

    kafka Json bean 등록 produce consume

--- API/src/main/java/hello/api/service/KafkaConsumer.java
+++ API/src/main/java/hello/api/service/KafkaConsumer.java
@@ -1,16 +1,24 @@
 package hello.api.service;
 
 import hello.api.dto.EmailDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
 @Service
+@Slf4j
 public class KafkaConsumer {
 
 //    @KafkaListener(topics = "exam", groupId = "foo")
 //    public void consume(EmailDto emailDto) throws IOException {
 //        System.out.println(String.format("Consumed message : %s", emailDto));
 //    }
+
+    @KafkaListener(topics = "exam", groupId = "foo", containerFactory = "stockChangeListener")
+    public void consume(EmailDto emailDto) {
+
+        log.info("email consume={}", emailDto);
+    }
 }
--- API/src/main/java/hello/api/service/KafkaProducer.java
+++ API/src/main/java/hello/api/service/KafkaProducer.java
@@ -1,19 +1,29 @@
 package hello.api.service;
 import hello.api.dto.EmailDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class KafkaProducer {
-    private static final String TOPIC = "newnew";
-    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private static final String TOPIC = "newexam";
+    private final KafkaTemplate<String, EmailDto> kafkaTemplate;
+//    private final KafkaTemplate<String, Object> kafkaTemplate;
+//
+//    public void sendMessage(String message) {
+//        EmailDto emailDto = new EmailDto(message);
+//        System.out.println(String.format("Produce message : %s", emailDto));
+//        this.kafkaTemplate.send(TOPIC, emailDto);
+//    }
 
     public void sendMessage(String message) {
         EmailDto emailDto = new EmailDto(message);
-        System.out.println(String.format("Produce message : %s", emailDto));
-        this.kafkaTemplate.send(TOPIC, emailDto);
+        // Send a message
+        log.info("produce message={}", emailDto);
+        kafkaTemplate.send(TOPIC, emailDto);
     }
 }
