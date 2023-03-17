5b230029cbb835e111875c00a6caa95c4f1f037c
geung <geungan9@gmail.com>
2023-03-17 14:41:40 +0900

    본 Api kafka 인증할 메일 발송 기능 적용

--- API/src/main/java/hello/api/dto/KafkaEmailDto.java
+++ API/src/main/java/hello/api/dto/KafkaEmailDto.java
@@ -1,11 +1,18 @@
 package hello.api.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class KafkaEmailDto {
 
-    private String email;
+    String email;
 }
