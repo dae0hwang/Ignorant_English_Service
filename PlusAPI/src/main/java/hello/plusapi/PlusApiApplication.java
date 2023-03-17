38e08b21504a9826672f850a58dc835564d3fec1
geung <geungan9@gmail.com>
2023-03-17 22:31:12 +0900

    이메일 통계 추가 서버 하위 모듈 등록

---
+++ PlusAPI/src/main/java/hello/plusapi/PlusApiApplication.java
@@ -0,0 +1,13 @@
+package hello.plusapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlusApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlusApiApplication.class, args);
+    }
+
+}
---
+++ PlusAPI/src/test/java/hello/plusapi/PlusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.plusapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlusApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
