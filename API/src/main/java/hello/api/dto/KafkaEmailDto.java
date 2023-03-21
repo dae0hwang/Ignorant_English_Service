5c081d368640d6cec42a6a6e4ce5454b63ae0499
geung <geungan9@gmail.com>
2023-03-21 09:24:36 +0900

    선생님이 문제해결해주심

--- API/src/main/java/hello/api/dto/KafkaEmailDto.java
+++ API/src/main/java/hello/api/dto/KafkaEmailDto.java
@@ -6,12 +6,14 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.stereotype.Service;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class KafkaEmailDto {
 
     String email;
