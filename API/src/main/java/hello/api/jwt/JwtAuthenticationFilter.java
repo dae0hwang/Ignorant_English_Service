b8c24e83b2638e6272e09eebc1be8bd43add9378
geung <geungan9@gmail.com>
2023-03-30 13:36:09 +0900

    Spring Security JWT토큰 처리 주석 설명 추가

--- API/src/main/java/hello/api/jwt/JwtAuthenticationFilter.java
+++ API/src/main/java/hello/api/jwt/JwtAuthenticationFilter.java
@@ -18,21 +18,20 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-// /login으로 인증 요청시
+// API/login url로 post 요청을 하게 되면, 해당 필터가 실행된다.
 @RequiredArgsConstructor
 @Slf4j
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
 
 	private final AuthenticationManager authenticationManager;
-	
-	// Authentication 객체 만들어서 리턴 => 의존 : AuthenticationManager
-	// 인증 요청시에 실행되는 함수 => /login
+
 	@Override
+	// Authentication 객체 만들어서 리턴해야 한다.(AuthenticationManager를 통해서)
 	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
-			throws AuthenticationException {
+		throws AuthenticationException {
 
 		log.info("JwtAuthenticationFilter 로그인 : 진입");
-
+		//로그인 요청 시 들어온 데이터를 객체로 변환
 		ObjectMapper om = new ObjectMapper();
 		UserLoginRequest userLoginRequest = null;
 		try {
@@ -41,46 +40,41 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 			e.printStackTrace();
 		}
 
+		//해당 객체로 로그인 시도를 위한 유저네임패스워드 authenticationToken 생성
+		UsernamePasswordAuthenticationToken authenticationToken =
+			new UsernamePasswordAuthenticationToken(
+				userLoginRequest.getUsername(),
+				userLoginRequest.getPassword());
 
-		// json 데이트를 통해서 로그인 시도를 위한 유저네임패스워드 토큰 생성
-		UsernamePasswordAuthenticationToken authenticationToken = 
-				new UsernamePasswordAuthenticationToken(
-					userLoginRequest.getUsername(),
-					userLoginRequest.getPassword());
-		
 
 		// authenticate() 함수가 호출 되면 인증 프로바이더가 유저 디테일 서비스의
 		// loadUserByUsername(토큰의 첫번째 파라미터) 를 호출하고
 		// UserDetails를 리턴받아서 토큰의 두번째 파라메터(credential)과
 		// UserDetails(DB값)의 getPassword()함수로 비교해서 동일하면
 		// Authentication 객체를 만들어서 필터체인으로 리턴해준다.
-		
+		Authentication authentication =
+			authenticationManager.authenticate(authenticationToken);
 		// Tip: 인증 프로바이더의 디폴트 서비스는 UserDetailsService 타입
 		// Tip: 인증 프로바이더의 디폴트 암호화 방식은 BCryptPasswordEncoder
 		// 결론은 인증 프로바이더에게 알려줄 필요가 없음.
 
-		//로그인 시도를 하려고  여기다가 토큰을 날림
-		Authentication authentication = 
-				authenticationManager.authenticate(authenticationToken);
-		//위 영역이 성공했다면, session영역에 authenticaion객체가 저장된다. ->로그인이 성공
+		//위 영역이 성공했다면, session영역에 authenticaion 객체가 저장된다->로그인이 성공
 		return authentication;
 	}
 
-	// 인증 성공했다면, JWT Token 생성해서 response에 담아주기
 	@Override
+	// 로그인 인증 성공하면 들어오는 메소드
 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
-			Authentication authResult) throws IOException, ServletException {
-		
+		Authentication authResult) throws IOException, ServletException {
+		//Authentication에 있는 정보로 JWT Token 생성해서 response에 담아주기
 		PrincipalDetails principalDetailis = (PrincipalDetails) authResult.getPrincipal();
-		
 		String jwtToken = JWT.create()
-				.withSubject(principalDetailis.getUsername())
-				.withExpiresAt(new Date(System.currentTimeMillis()+JwtProperties.EXPIRATION_TIME))
-				.withClaim("id", principalDetailis.getUser().getId())
-				.withClaim("username", principalDetailis.getUser().getUsername())
-				.sign(Algorithm.HMAC512(JwtProperties.SECRET));
-		
+			.withSubject(principalDetailis.getUsername())
+			.withExpiresAt(new Date(System.currentTimeMillis()+JwtProperties.EXPIRATION_TIME))
+			.withClaim("id", principalDetailis.getUser().getId())
+			.withClaim("username", principalDetailis.getUser().getUsername())
+			.sign(Algorithm.HMAC512(JwtProperties.SECRET));
+
 		response.addHeader(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX+jwtToken);
 	}
-	
 }
--- API/src/main/java/hello/api/jwt/JwtAuthorizationFilter.java
+++ API/src/main/java/hello/api/jwt/JwtAuthorizationFilter.java
@@ -30,19 +30,19 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
+		throws IOException, ServletException {
 		log.info("JwtAuthorizationFilter 인가 : 진입");
 
+		//JWT 토큰이 헤더가 존재한다면, if문 안으로 들어가서 토큰 검증을 시작한다.
 		String header = request.getHeader(JwtProperties.HEADER_STRING);
-
 		if (header != null && header.startsWith(JwtProperties.TOKEN_PREFIX)) {
 			System.out.println("header : " + header);
 			String token = request.getHeader(JwtProperties.HEADER_STRING)
 				.replace(JwtProperties.TOKEN_PREFIX, "");
 
-			// 토큰 검증 (이게 인증이기 때문에 AuthenticationManager도 필요 없음)
-			// 내가 SecurityContext에 집적접근해서 세션을 만들때 자동으로 UserDetailsService에 있는
-			// loadByUsername이 호출됨.
+			// 토큰 자체 검증
+			// 토큰 자체에 인가 정보가 담겨 있기 때문에 AuthenticationManager가 필요 없다.
+			//내가 가지고 있는 secret키를 사용해, JWT 토큰이 올바른 지 확인
 			String username = JWT.require(Algorithm.HMAC512(JwtProperties.SECRET)).build().verify(token)
 				.getClaim("username").asString();
 
@@ -50,7 +50,7 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 				Users user = userRepository.findByUsername(username).orElseThrow();
 
 				// 인증은 토큰 검증시 끝. 인증을 하기 위해서가 아닌 스프링 시큐리티가 수행해주는 권한 처리를 위해
-				// 아래와 같이 토큰을 만들어서 Authentication 객체를 강제로 만들고 그걸 세션에 저장!
+				// 아래와 같이 토큰을 만들어서 Authentication 객체를 강제로 만들고 그걸 세션에 저장하기!!
 				PrincipalDetails principalDetails = new PrincipalDetails(user);
 				Authentication authentication = new UsernamePasswordAuthenticationToken(
 					principalDetails, // 나중에 컨트롤러에서 DI해서 쓸 때 사용하기 편함.
@@ -62,6 +62,9 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 				SecurityContextHolder.getContext().setAuthentication(authentication);
 			}
 		}
+		//다음 과정으로 넘어간다.
+		//JWT 토큰이 맞다면 Security 세션에 값이 있는 상태에서 로그인 상태로 다음 절차 진행
+		//JWT 토큰 인증 실패했다면, Security 세션이 빈 상태로 다음 절차 진행 -> 에러 발생
 		doFilter(request, response, chain);
 	}
 }
