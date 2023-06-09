2dca0da310837c4d93f0fc6b3a4588835d937b30
geung <geungan9@gmail.com>
2023-06-09 09:45:09 +0900

    feat: change property for aws
    api ui plus api application.yml 변경
    api ui plus api dockerfile 수정
    api ui plus api jenkinsfile 수정
    rds만 aws 적용

---
+++ PlusAPI/JenkinsFile-aws.groovy
@@ -0,0 +1,54 @@
+pipeline {
+    agent any
+    environment {
+        registryCredential = 'DockerHub_IdPwd'
+        dockerImage = ''
+    }
+
+    stages {
+        // git에서 repository clone
+        stage('Prepare') {
+            steps {
+                echo 'Clonning Repository'
+                git url: 'https://github.com/dae0hwang/Ignorant_English_Service',
+                        branch: 'master'
+            }
+        }
+
+        // gradle build
+        stage('Bulid Gradle') {
+            agent any
+            steps {
+                echo 'Bulid Gradle'
+                //이부분
+                dir ('../PlusAPI/PlusAPI') {
+                    bat 'gradlew clean build'
+                }
+            }
+        }
+        // docker build
+        stage('Bulid Docker') {
+            agent any
+            steps {
+                echo 'Bulid Docker'
+                //이부분
+                dir ('../PlusAPI/PlusAPI') {
+                    script {
+                        dockerImage = docker.build 'hwangdy/plus:latest'
+                    }
+                }
+            }
+        }
+        // docker push
+        stage('Push Docker') {
+            steps {
+                echo 'Push Docker'
+                script {
+                    docker.withRegistry('', registryCredential) {
+                        dockerImage.push()
+                    }
+                }
+            }
+        }
+    }
+}
