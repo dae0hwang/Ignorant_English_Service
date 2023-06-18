6c9850b2f3957d2df9030e8c991d74a8956ea1a2
geung <geungan9@gmail.com>
2023-06-18 20:31:19 +0900

    refacotr: aws -loadbalance api plus api setting

--- argo/argo-jenkins.groovy
+++ argo/argo-jenkins.groovy
@@ -51,117 +51,119 @@ pipeline {
                 }
             }
         }
-//
-//        // 2 PLUS 서버 진행
-//        // gradle build
-//        stage('PLUS Bulid Gradle') {
-//            agent any
-//            steps {
-//                echo 'Bulid Gradle'
-//                //이부분
-//                dir ('../argo-english/PLUS') {
-//                    bat 'gradlew clean build'
-//                }
-//            }
-//        }
-//        // docker build
-//        stage('PLUS Bulid Docker') {
-//            agent any
-//            steps {
-//                echo 'Bulid Docker'
-//                dir ('../argo-english/PLUS') {
-//                    script {
-//                        dockerImage = docker.build "${plusImageName}"
-//                    }
-//                }
-//            }
-//        }
-//        // docker push
-//        stage('PLUS Push Docker') {
-//            steps {
-//                echo 'Push Docker'
-//                script {
-//                    docker.withRegistry('', registryCredential) {
-//                        dockerImage.push()
-//                    }
-//                }
-//            }
-//        }
-//
-//        // 3 UI 서버 진행
-//        // gradle build
-//        stage('UI Bulid Gradle') {
-//            agent any
-//            steps {
-//                echo 'Bulid Gradle'
-//                //이부분
-//                dir ('../argo-english/UI') {
-//                    bat 'gradlew clean build'
-//                }
-//            }
-//        }
-//        // docker build
-//        stage('UI Bulid Docker') {
-//            agent any
-//            steps {
-//                echo 'Bulid Docker'
-//                dir ('../argo-english/UI') {
-//                    script {
-//                        dockerImage = docker.build "${uiImageName}"
-//                    }
-//                }
-//            }
-//        }
-//        // docker push
-//        stage('UI Push Docker') {
-//            steps {
-//                echo 'Push Docker'
-//                script {
-//                    docker.withRegistry('', registryCredential) {
-//                        dockerImage.push()
-//                    }
-//                }
-//            }
-//        }
-//
-//
-//
-//        // git에서 가져오기
-//        stage('Prepare Helm') {
-//            steps {
-//                dir ('../argo-english@tmp') {
-//                    git url: 'https://github.com/dae0hwang/helm-chart', branch: 'master'
-//                }
-//            }
-//        }
-//        //value.yaml 파일을 변경을 해줘야한다.
-//        stage('update Helm') {
-//            steps {
-//                dir ('../argo-english@tmp/helm-english') {
-//                    script {
-//                        def filename = 'values.yaml'
-//                        def data = readYaml file: filename
-//                        data.image.api= "${apiImageName}"
-//                        data.image.ui= "${uiImageName}"
-//                        data.image.plus= "${plusImageName}"
-//                        writeYaml file: filename, data: data, overwrite: true
-//                    }
-//                }
-//            }
-//        }
-//
-//        stage('push Helm to git') {
-//            steps {
-//                dir ('../argo-english@tmp') {
-//                    withCredentials([gitUsernamePassword(credentialsId: 'git-hub-credential', gitToolName: 'git-tool')]) {
-//                        bat 'git add .'
-//                        bat 'git config --global user.email "geungan9@gmail.com"'
-//                        bat 'git config --global user.name "dae0hwang"'
-//                        bat "git commit -m argo"
-//                        bat 'git push --set-upstream origin master'
-//                    }
-//                }
-//            }
-//        }
+
+        // 2 PLUS 서버 진행
+        // gradle build
+        // gradle build
+        stage('PLUS Bulid Gradle') {
+            agent any
+            steps {
+                echo 'Bulid Gradle'
+                //이부분
+                dir ('../argo-english/PlusAPI') {
+                    bat 'gradlew clean build'
+                }
+            }
+        }
+
+        // docker build
+        stage('PLUS Bulid Docker') {
+            agent any
+            steps {
+                echo 'Bulid Docker'
+                dir ('../argo-english/PlusAPI') {
+                    script {
+                        dockerImage = docker.build "${plusImageName}"
+                    }
+                }
+            }
+        }
+        // docker push
+        stage('PLUS Push Docker') {
+            steps {
+                echo 'Push Docker'
+                script {
+                    docker.withRegistry('', registryCredential) {
+                        dockerImage.push()
+                    }
+                }
+            }
+        }
+
+        // 3 UI 서버 진행
+        // gradle build
+        stage('UI Bulid Gradle') {
+            agent any
+            steps {
+                echo 'Bulid Gradle'
+                //이부분
+                dir ('../argo-english/UI') {
+                    bat 'gradlew clean build'
+                }
+            }
+        }
+        // docker build
+        stage('UI Bulid Docker') {
+            agent any
+            steps {
+                echo 'Bulid Docker'
+                dir ('../argo-english/UI') {
+                    script {
+                        dockerImage = docker.build "${uiImageName}"
+                    }
+                }
+            }
+        }
+        // docker push
+        stage('UI Push Docker') {
+            steps {
+                echo 'Push Docker'
+                script {
+                    docker.withRegistry('', registryCredential) {
+                        dockerImage.push()
+                    }
+                }
+            }
+        }
+
+
+
+        // git에서 가져오기
+        stage('Prepare Helm') {
+            steps {
+                dir ('../argo-english@tmp') {
+                    git url: 'https://github.com/dae0hwang/helm-chart', branch: 'master'
+                }
+            }
+        }
+        //value.yaml 파일을 변경을 해줘야한다.
+        stage('update Helm') {
+            steps {
+                dir ('../argo-english@tmp/helm-english') {
+                    script {
+                        def filename = 'values.yaml'
+                        def data = readYaml file: filename
+                        data.image.api= "${apiImageName}"
+                        data.image.ui= "${uiImageName}"
+                        data.image.plus= "${plusImageName}"
+                        writeYaml file: filename, data: data, overwrite: true
+                    }
+                }
+            }
+        }
+
+        stage('push Helm to git') {
+            steps {
+                dir ('../argo-english@tmp') {
+                    withCredentials([gitUsernamePassword(credentialsId: 'git-hub-credential', gitToolName: 'git-tool')]) {
+                        bat 'git add .'
+                        bat 'git config --global user.email "geungan9@gmail.com"'
+                        bat 'git config --global user.name "dae0hwang"'
+                        bat "git commit -m argo"
+                        bat 'git push --set-upstream origin master'
+                    }
+                }
+            }
+        }
     }
 }
